9e4de7f8e504e9154d2dbe4e63453099fbbe7bbc
MissSilvia88 <94132716+MissSilvia88@users.noreply.github.com>
2021-11-11 18:34:13 +0000

    Check this one

--- src/test/java/com/tenniscourts/reservations/ReservationServiceTest.java
+++ src/test/java/com/tenniscourts/reservations/ReservationServiceTest.java
@@ -14,6 +14,11 @@ import org.springframework.test.context.ContextConfiguration;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import java.util.Scanner;
+import com.tenniscourts.Schedule;
+import java.util.*;
+
+
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @SpringBootTest
 @RunWith(MockitoJUnitRunner.class)
@@ -23,14 +28,24 @@ public class ReservationServiceTest {
     @InjectMocks
     ReservationService reservationService;
 
-    @Test
-    public void getRefundValueFullRefund() {
+    @Silvia
+  
+
+		public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
         Schedule schedule = new Schedule();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/mm/yyyy");
 
         LocalDateTime startDateTime = LocalDateTime.now().plusDays(2);
 
-        schedule.setStartDateTime(startDateTime);
-
+        {schedule.setStartDateTime(startDateTime);
         Assert.assertEquals(reservationService.getRefundValue(Reservation.builder().schedule(schedule).value(new BigDecimal(10L)).build()), new BigDecimal(10));
+       
+        sc.close();
+	
+	
+
+        
     }
 }
+
