9aadcc6e2ed77937a11163db46659010238889af
MissSilvia88 <94132716+MissSilvia88@users.noreply.github.com>
2021-11-14 23:27:58 +0000

    Update SwaggerConfig.java

--- src/main/java/com/tenniscourts/config/swagger/SwaggerConfig.java
+++ src/main/java/com/tenniscourts/config/swagger/SwaggerConfig.java
@@ -1,21 +1,69 @@
+SwaggerConfig.java
+
 package com.tenniscourts.config.swagger;
 
+ 
+
+import com.fasterxml.classmate.TypeResolver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+
 import springfox.documentation.builders.PathSelectors;
+
 import springfox.documentation.builders.RequestHandlerSelectors;
+
+import springfox.documentation.service.ApiInfo;
+
 import springfox.documentation.spi.DocumentationType;
+
 import springfox.documentation.spring.web.plugins.Docket;
 
+ 
 
 @Configuration
+
 public class SwaggerConfig {
 
+ 
+
+    @SuppressWarnings("unused")
+
+    @Autowired
+
+    private TypeResolver typeResolver;
+
+ 
+
     @Bean
+
     public Docket productApi() {
+
         return new Docket(DocumentationType.SWAGGER_2)
+
                 .select().apis(RequestHandlerSelectors.basePackage("com.tenniscourts"))
-                .paths(PathSelectors.any())
-                .build();
+
+                .paths(PathSelectors.ant("/*"))
+
+                .build().apiInfo(apiInfo());
+
     }
+
+ 
+
+    private ApiInfo apiInfo() {
+
+        return new ApiInfoBuilder().title("Tennis Courts").description(
+
+                "Tennis Courts is a backend API for a simple reservation platform for tennis players")
+
+                .version("1.0").build();
+
+    }
+
 }
